00313910ca763e7713591f5e056ea82032a9649f
Harry Winer <winer.harry@gmail.com>
2022-09-22 12:54:46 +0100

    added simple-statistics testing code

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+var parse = require('csv-parse').parse;
 
 const express = require('express')
 const app = express()
@@ -6,6 +7,7 @@ const path = require('path')
 const port = 3000
 
 const classifiers = require('./src/classifiers')
+
 
 /**
  * A simple server to serve Chart.js charts 
@@ -15,12 +17,24 @@ const classifiers = require('./src/classifiers')
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "charts/alldata.html"))
 })
+app.get('/iris', (req, res) => {
+  res.sendFile(path.join(__dirname, "charts/iris.html"))
+})
 
 app.use('/classifiers', classifiers)
 
 app.get('/devices', (req, res) => {
     let rawdata = fs.readFileSync('json/devices.json');
     res.send(JSON.parse(rawdata));
+})
+app.get('/irisdata', (req, res) => {
+    var rawdata = fs.readFileSync('json/iris.csv');
+    parse(rawdata, {
+        comment: '#'
+      }, function(err, records){
+        console.error(err)
+        res.send(records.slice(1).filter(e => e[4] == "Setosa").map(e => e.slice(0,4).map(i => parseFloat(i))))
+      });
 })
 
 app.get('/')
